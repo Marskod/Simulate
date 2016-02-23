04169df4829501061520cd7db921d8538f78c894
jaybutera <buterjay@gmail.com>
2016-02-23 00:24:58 -0600

    Add sound and adjust parameters

--- liftSphere.cs
+++ liftSphere.cs
@@ -6,7 +6,8 @@ public class liftSphere : MonoBehaviour {
     public float height = 2.46f;
 
     private float baseHeight;
-    public int gain = 700;
+    public int gain;
+    public int gain_audio = 10; 
 
     public float smoothness = 2;
 
@@ -23,14 +24,15 @@ public class liftSphere : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (transform.position.y > 75)
-            transform.position = new Vector3(transform.position.x, 74, transform.position.z);
+            transform.position = new Vector3(transform.position.x, 2, transform.position.z);
         //for (int i = 40; i < 3; i++)
           // alpha += gameObject.GetComponent<grabJson>().values[i]; // ~10hz avg
         //alpha /= 3.0f;
-        alpha = gameObject.GetComponent<grabJson>().values[41];
+        alpha = gameObject.GetComponent<grabJson>().values[41] + gameObject.GetComponent<grabJson>().values[42] + gameObject.GetComponent<grabJson>().values[40];
+        alpha /= 3f;
         //Debug.Log(alpha);
-        lowBound = gameObject.GetComponent<grabJson>().values[28]; // Corresponds to ~7hz
-        highBound = gameObject.GetComponent<grabJson>().values[53]; // Corresponds to ~13hz
+        lowBound = gameObject.GetComponent<grabJson>().values[18]; // Corresponds to ~7hz
+        highBound = gameObject.GetComponent<grabJson>().values[55]; // Corresponds to ~13hz
 
         /*if (height >= (baseHeight+1))
             height = (baseHeight + 1);
@@ -38,8 +40,17 @@ public class liftSphere : MonoBehaviour {
             height = (baseHeight - 1);
         */
 
+        /*
+        gain = gameObject.GetComponent<calibrateGain>().getGain(.5f);
+
+        if (gain == null)
+            gain = 0;
+        Debug.Log(gain);
+        */
 
         //height = val * offset;
+
+        gameObject.GetComponent<AudioSource>().pitch = (transform.position.y)/25;
 
         // Find difference between alpha band and adjacent frequencies
         float diff =  Mathf.Pow(alpha,3) - (lowBound + highBound) / 2.0f;
