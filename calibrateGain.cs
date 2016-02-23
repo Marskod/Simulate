7dfc56835e3a0c925b0bd9ea52d68eb028efec47
jaybutera <buterjay@gmail.com>
2016-02-23 00:25:28 -0600

    Scriptually determine gain parameter

---
+++ calibrateGain.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class calibrateGain : MonoBehaviour {
+
+    List<float> samples;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public int getGain(float sampRate) {
+        float sampPeriod = 3; // 3 second sampling period
+
+        InvokeRepeating("sampleAlpha", 0, sampRate);
+
+        // Wait for sample data to populate
+        StartCoroutine(wait(sampPeriod));
+
+        CancelInvoke("sampleAlpha");
+
+        return calcGain();
+    }
+
+    void sampleAlpha () {
+        samples.Add(gameObject.GetComponent<grabJson>().values[41]);
+    }
+
+    int calcGain () {
+        double objMass = 1.0;
+
+        double avg = samples.Average();
+
+        // Since [ avg * gain = objMass * gravity_acc ] makes the object stable
+        return (int)( (objMass * 9.81) / avg);
+    }
+
+    IEnumerator wait(float t)
+    {
+        yield return new WaitForSeconds(t);
+    }
+}
